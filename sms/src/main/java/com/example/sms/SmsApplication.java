9b9f93a9c16fe1f742379d7f623ffec58b1399cf
alxayeed <alxayeed@gmail.com>
2021-08-25 11:49:22 +0600

    command line runner added to add data to db

--- sms/src/main/java/com/example/sms/SmsApplication.java
+++ sms/src/main/java/com/example/sms/SmsApplication.java
@@ -1,13 +1,27 @@
 package com.example.sms;
 
+import com.example.sms.entity.Student;
+import com.example.sms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmsApplication {
+public class SmsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
+
+	public void run(String... args) throws Exception{
+		Student student1 = new Student("Al", "Xayeed", "alxayeed@gmail.com");
+		studentRepository.save(student1);
+
+		Student student2 = new Student("Abdullah", "Sayeed", "alsayeed@yahoo.com");
+		studentRepository.save(student2);
+	}
 }
